fed9be7195e7b6dc3fc0f8048763fa42d7199d9f
Lital Levin <lital.levin@gmail.com>
2020-07-08 15:53:26 +0200

    navbar fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
   render() {
     return (
       <div className="full-width-container">
-        <div className="navbar-top level is-mobile margin-bottom-0">
+        <div className="navbar-top level is-mobile">
           <div className="level-left">
             <div className="navbar-item logo">
               <Link to="/" title="Logo">
@@ -51,41 +51,40 @@ const Navbar = class extends React.Component {
             </div>
           </div>
 
-          <div className="level-right is-hidden-mobile">
-            <div className="level-item no-flex">
+          <div className="level-right is-hidden-mobile info-container">
+            <div className="info-tile">
+              <span className="icon">
+                <FontAwesomeIcon icon={faPhone} />
+              </span>
               <p className="heading">
-                <span className="icon">
-                  <FontAwesomeIcon icon={faPhone} />
-                </span>
                 Nous contacter
+                <br />
+                01 46 28 65 16
               </p>
-              <p className="heading margin-left-1">01 46 28 65 16</p>
             </div>
 
-            <div className="level-item no-flex">
+            <div className="info-tile">
+              <span className="icon">
+                <FontAwesomeIcon icon={faClock} />
+              </span>
               <p className="heading">
-                <span className="icon">
-                  <FontAwesomeIcon icon={faClock} />
-                </span>
                 Mardi-Samedi
+                <br />
+                9h15-13h et 14h-19h 
               </p>
-              <p className="heading margin-left-1"> 9h15-13h et 14h-19h </p>
+              <p className="heading"> </p>
             </div>
 
-            <div
-              className="level-item no-flex address"
-              style={{ borderRight: 5, marginRight: 25 }}
-            >
-              <Link to="/" className="heading has-text-grey-dark">
-                <span className="icon">
-                  <FontAwesomeIcon icon={faMapMarker} />
-                </span>
-                2 rue Beccaria
-                <p className="heading margin-left-1">75012 Paris</p>
-              </Link>
-            </div>
+            <Link to="/" className="info-tile">
+              <span className="icon">
+                <FontAwesomeIcon icon={faMapMarker} />
+              </span>
 
-            <p className="level-item heading"></p>
+              <p className="heading">
+                2 rue Beccaria <br />
+                75012 Paris
+              </p>
+            </Link>
           </div>
         </div>
 
@@ -113,19 +112,31 @@ const Navbar = class extends React.Component {
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
             <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                STUDIO
-              </Link>
-              <Link className="navbar-item" to="/products">
+              <div className="navbar-item has-dropdown is-hoverable">
+                <Link className="navbar-link" to="/studio">
+                  STUDIO
+                </Link>
+
+                <div className="navbar-dropdown">
+                  <Link className="navbar-item" to="/photoID">
+                    Photo d'identité
+                  </Link>
+                  <Link className="navbar-item" to="/portrait">
+                    Portrait
+                  </Link>
+                </div>
+              </div>
+
+              <Link className="navbar-item" to="/labo">
                 LABORATOIRE
               </Link>
-              <Link className="navbar-item" to="/blog">
+              <Link className="navbar-item" to="/encadrement">
                 ENCADREMENT
               </Link>
-              <Link className="navbar-item" to="/contact">
+              <Link className="navbar-item" to="/transfervideo">
                 TRANSDER VIDEO
               </Link>
-              <Link className="navbar-item" to="/contact/examples">
+              <Link className="navbar-item" to="/boutique">
                 EN BOUTIQUE
               </Link>
             </div>
@@ -138,46 +149,41 @@ const Navbar = class extends React.Component {
                 INFO PRATIQUE
               </Link>
 
-                <div className="level is-hidden-tablet has-text-centered">
-                  <hr/>
+              <div className="level is-hidden-tablet has-text-centered">
+                <hr className="" />
 
-                  <div className="level-item has-text-grey-dark no-flex">
-                    <p className="heading has-text-grey-dark">
-                      <span className="icon">
-                        <FontAwesomeIcon icon={faPhone} />
-                      </span>
-                      01 46 28 65 16
-                    </p>
-
-                  </div>
-
-                  <div className="level-item has-text-grey-dark no-flex">
-                    <p className="heading">
-                      <span className="icon">
-                        <FontAwesomeIcon icon={faClock} />
-                      </span>
-                      Mar-Sam 9h15-13h et 14h-19h  (sans RDV)
-                    </p>
-                    <p className="heading">
-                      {" "}
-
-                    </p>
-                  </div>
-
-                  <div
-                    className="level-item no-flex has-text-grey-dark address">
-                    <Link to="/" className="heading has-text-grey-dark">
-                      <span className="icon">
-                        <FontAwesomeIcon icon={faMapMarker} />
-                      </span>
-                      2 rue Beccaria, 75012 Paris
-                    </Link>
-                  </div>
-
-                  <p className="level-item heading"></p>
+                <div className="level-item has-text-grey-dark no-flex">
+                  <p className="heading has-text-grey-dark">
+                    <span className="icon">
+                      <FontAwesomeIcon icon={faPhone} />
+                    </span>
+                    01 46 28 65 16
+                  </p>
                 </div>
+
+                <div className="level-item has-text-grey-dark no-flex">
+                  <p className="heading">
+                    <span className="icon">
+                      <FontAwesomeIcon icon={faClock} />
+                    </span>
+                    Mar-Sam 9h15-13h et 14h-19h  (sans RDV)
+                  </p>
+                  <p className="heading"> </p>
+                </div>
+
+                <div className="level-item no-flex has-text-grey-dark address">
+                  <Link to="/" className="heading has-text-grey-dark">
+                    <span className="icon">
+                      <FontAwesomeIcon icon={faMapMarker} />
+                    </span>
+                    2 rue Beccaria, 75012 Paris
+                  </Link>
+                </div>
+
+                <p className="level-item heading"></p>
               </div>
             </div>
+          </div>
         </nav>
       </div>
     );
---
+++ src/pages/studio/index.md
@@ -0,0 +1,22 @@
+---
+templateKey: 'studio-page'
+path: /studio
+title: Studio
+image: /img/studio.jpeg
+heading:  Spécialiste du portrait
+description: >-
+  Photo Expression réalise de très beaux portraits artistiques, retouchés pour CV et réseaux sociaux en adéquation avec votre profil.
+main:
+  heading: Pour Professionnels et Particuliers (adultes, enfants, bébés)
+  description: >
+    Portrait De famille ou de groupe, d'entreprise, pour strobinoscyse, en pieds et en buste pour CV de compagnie aérienne. Photos techniques pour usage médical ou chirurgie esthétique et réparatrice
+  image1:
+    alt: A close-up of a paper filter filled with ground coffee
+    image: /img/products-grid3.jpg
+  image2:
+    alt: A green cup of a coffee on a wooden table
+    image: /img/products-grid2.jpg
+  image3:
+    alt: Coffee beans
+    image: /img/products-grid1.jpg
+---
