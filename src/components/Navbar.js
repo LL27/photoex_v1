487ef28fed86090a4eac5b01f5628b5d7a43a643
Lital Levin <lital.levin@gmail.com>
2020-07-09 11:56:15 +0200

    navbar improvements

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,49 +42,51 @@ const Navbar = class extends React.Component {
   render() {
     return (
       <div className="full-width-container">
-        <div className="navbar-top level is-mobile">
-          <div className="level-left">
-            <div className="navbar-item logo">
-              <Link to="/" title="Logo">
-                <img src={logo} alt="Photo Expression" className="logo-svg" />
+        <div className="">
+          <div className="navbar-top level is-mobile">
+            <div className="level-left">
+              <div className="navbar-item logo">
+                <Link to="/" title="Logo">
+                  <img src={logo} alt="Photo Expression" className="logo-svg" />
+                </Link>
+              </div>
+            </div>
+
+            <div className="level-right is-hidden-mobile info-container">
+              <div className="info-tile">
+                <span className="icon">
+                  <FontAwesomeIcon icon={faPhone} />
+                </span>
+                <a href="tel:0146286516" className="heading address">
+                  Nous contacter
+                  <br />
+                  01 46 28 65 16
+                </a>
+              </div>
+
+              <div className="info-tile">
+                <span className="icon">
+                  <FontAwesomeIcon icon={faClock} />
+                </span>
+                <p className="heading">
+                  Mardi-Samedi
+                  <br />
+                  9h15-13h et 14h-19h 
+                </p>
+                <p className="heading"> </p>
+              </div>
+
+              <Link to="/" className="info-tile">
+                <span className="icon">
+                  <FontAwesomeIcon icon={faMapMarker} />
+                </span>
+
+                <p className="heading address">
+                  2 rue Beccaria <br />
+                  75012 Paris
+                </p>
               </Link>
             </div>
-          </div>
-
-          <div className="level-right is-hidden-mobile info-container">
-            <div className="info-tile">
-              <span className="icon">
-                <FontAwesomeIcon icon={faPhone} />
-              </span>
-              <p className="heading">
-                Nous contacter
-                <br />
-                01 46 28 65 16
-              </p>
-            </div>
-
-            <div className="info-tile">
-              <span className="icon">
-                <FontAwesomeIcon icon={faClock} />
-              </span>
-              <p className="heading">
-                Mardi-Samedi
-                <br />
-                9h15-13h et 14h-19h 
-              </p>
-              <p className="heading"> </p>
-            </div>
-
-            <Link to="/" className="info-tile">
-              <span className="icon">
-                <FontAwesomeIcon icon={faMapMarker} />
-              </span>
-
-              <p className="heading">
-                2 rue Beccaria <br />
-                75012 Paris
-              </p>
-            </Link>
           </div>
         </div>
 
@@ -93,94 +95,103 @@ const Navbar = class extends React.Component {
           role="navigation"
           aria-label="main-navigation"
         >
-          {/* Hamburger menu */}
-          <div className="navbar-brand">
-            <div></div>
-            <div
-              className={`navbar-burger burger ${this.state.navBarActiveClass}`}
-              data-target="navMenu"
-              onClick={() => this.toggleHamburger()}
-            >
-              <span />
-              <span />
-              <span />
+          <div className="container">
+            {/* Hamburger menu */}
+            <div className="navbar-brand">
+              <div></div>
+              <div
+                className={`navbar-burger burger ${this.state.navBarActiveClass}`}
+                data-target="navMenu"
+                onClick={() => this.toggleHamburger()}
+              >
+                <span />
+                <span />
+                <span />
+              </div>
             </div>
-          </div>
 
-          <div
-            id="navMenu"
-            className={`navbar-menu ${this.state.navBarActiveClass}`}
-          >
-            <div className="navbar-start has-text-centered">
-              <div className="navbar-item has-dropdown is-hoverable">
-                <Link className="navbar-link" to="/studio">
-                  STUDIO
+            <div
+              id="navMenu"
+              className={`navbar-menu ${this.state.navBarActiveClass}`}
+            >
+              <div className="navbar-start has-text-centered">
+                <div className="navbar-item has-dropdown is-hoverable">
+                  <Link className="navbar-link" to="/studio">
+                    STUDIO
+                  </Link>
+
+                  <div className="navbar-dropdown">
+                    <Link className="navbar-item" to="/photoID">
+                      Photo d'identité
+                    </Link>
+                    <Link className="navbar-item" to="/portrait">
+                      Portrait
+                    </Link>
+                  </div>
+                </div>
+
+                <Link className="navbar-item" to="/labo">
+                  LABORATOIRE
+                </Link>
+                <Link className="navbar-item" to="/encadrement">
+                  ENCADREMENT
+                </Link>
+                <Link className="navbar-item" to="/transfervideo">
+                  TRANSDER VIDEO
+                </Link>
+                <Link className="navbar-item" to="/boutique">
+                  EN BOUTIQUE
+                </Link>
+              </div>
+              <div className="navbar-end has-text-centered">
+                <Link className="navbar-item" to="/contact/examples">
+                  <FontAwesomeIcon
+                    icon={faInfoCircle}
+                    style={{ marginRight: 5 }}
+                  />{" "}
+                  INFO PRATIQUE
                 </Link>
 
-                <div className="navbar-dropdown">
-                  <Link className="navbar-item" to="/photoID">
-                    Photo d'identité
-                  </Link>
-                  <Link className="navbar-item" to="/portrait">
-                    Portrait
-                  </Link>
+                <div className="level is-hidden-tablet has-text-centered">
+                  <hr className="" />
+
+                  <div className="level-item no-flex has-text-white-ter address">
+                    <Link
+                      to="/"
+                      className="heading has-text-white-ter"
+                      style={{ lineHeight: 2.2 }}
+                    >
+                      <span className="icon">
+                        <FontAwesomeIcon icon={faMapMarker} />
+                      </span>
+                      2 rue Beccaria, 75012 Paris
+                    </Link>
+                  </div>
+                  <div className="level-item has-text-white-ter no-flex address">
+                    <a
+                      href="tel:0146286516"
+                      className="heading has-text-white-ter"
+                      style={{ lineHeight: 2.2 }}
+                    >
+                      <span className="icon">
+                        <FontAwesomeIcon icon={faPhone} />
+                      </span>
+                      01 46 28 65 16
+                    </a>
+                  </div>
+
+                  <div className="level-item has-text-white-ter no-flex">
+                    <p className="heading" style={{ lineHeight: 2.2 }}>
+                      <span className="icon">
+                        <FontAwesomeIcon icon={faClock} />
+                      </span>
+                      Mar-Sam 9h15-13h et 14h-19h  (sans RDV)
+                    </p>
+                    <p className="heading"> </p>
+                  </div>
+
+                  <p className="level-item heading"></p>
                 </div>
-              </div>
-
-              <Link className="navbar-item" to="/labo">
-                LABORATOIRE
-              </Link>
-              <Link className="navbar-item" to="/encadrement">
-                ENCADREMENT
-              </Link>
-              <Link className="navbar-item" to="/transfervideo">
-                TRANSDER VIDEO
-              </Link>
-              <Link className="navbar-item" to="/boutique">
-                EN BOUTIQUE
-              </Link>
-            </div>
-            <div className="navbar-end has-text-centered">
-              <Link className="navbar-item" to="/contact/examples">
-                <FontAwesomeIcon
-                  icon={faInfoCircle}
-                  style={{ marginRight: 5 }}
-                />{" "}
-                INFO PRATIQUE
-              </Link>
-
-              <div className="level is-hidden-tablet has-text-centered">
-                <hr className="" />
-
-                <div className="level-item has-text-grey-dark no-flex">
-                  <p className="heading has-text-grey-dark">
-                    <span className="icon">
-                      <FontAwesomeIcon icon={faPhone} />
-                    </span>
-                    01 46 28 65 16
-                  </p>
-                </div>
-
-                <div className="level-item has-text-grey-dark no-flex">
-                  <p className="heading">
-                    <span className="icon">
-                      <FontAwesomeIcon icon={faClock} />
-                    </span>
-                    Mar-Sam 9h15-13h et 14h-19h  (sans RDV)
-                  </p>
-                  <p className="heading"> </p>
-                </div>
-
-                <div className="level-item no-flex has-text-grey-dark address">
-                  <Link to="/" className="heading has-text-grey-dark">
-                    <span className="icon">
-                      <FontAwesomeIcon icon={faMapMarker} />
-                    </span>
-                    2 rue Beccaria, 75012 Paris
-                  </Link>
-                </div>
-
-                <p className="level-item heading"></p>
               </div>
             </div>
           </div>
